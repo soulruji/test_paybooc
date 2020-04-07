848756ff883df0dfcddb40d37c83ac97544b94e1
soulruji <soulruji@gmail.com>
2020-04-07 15:35:35 +0900

    eject before

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./componenets/Banner";
+import Navigation from "./componenets/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <Navigation />
+        <Banner {...settings} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/componenets/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navigation() {
+	return (
+		<nav>
+			<ul>
+				<li><a href="#">#빨간날엔</a></li>
+				<li><a href="#">#맛집</a></li>
+				<li><a href="#">#쇼핑</a></li>
+				<li><a href="#">#여행</a></li>
+				<li><a href="#">#골프</a></li>
+				<li><a href="#">#생활구독</a></li>
+				<li><a href="#">#생활구독</a></li>
+				<li><a href="#">#핀테크</a></li>
+				<li><a href="#">#맞춤카드</a></li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navigation;
