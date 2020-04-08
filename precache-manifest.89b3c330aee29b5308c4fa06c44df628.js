b6a442e214c5a8882fc7da436465fe4fd5c3da0e
soulruji <soulruji@gmail.com>
2020-04-08 13:23:33 +0900

    Updates

---
+++ precache-manifest.89b3c330aee29b5308c4fa06c44df628.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0ca8ff890a20d956245dfe58b09e85",
+    "url": "/test_paybooc/index.html"
+  },
+  {
+    "revision": "0b43e1774d5f8afd50e1",
+    "url": "/test_paybooc/static/css/2.4aafeaf2.chunk.css"
+  },
+  {
+    "revision": "69d9a2238a0393d9f1d8",
+    "url": "/test_paybooc/static/css/main.8246afa0.chunk.css"
+  },
+  {
+    "revision": "0b43e1774d5f8afd50e1",
+    "url": "/test_paybooc/static/js/2.97e68809.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test_paybooc/static/js/2.97e68809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d9a2238a0393d9f1d8",
+    "url": "/test_paybooc/static/js/main.0e1604a6.chunk.js"
+  },
+  {
+    "revision": "eaf4bdcaa2a2792bf4a7",
+    "url": "/test_paybooc/static/js/runtime-main.b69ce0d1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/test_paybooc/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/test_paybooc/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/test_paybooc/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/test_paybooc/static/media/slick.f97e3bbf.svg"
+  }
+]);
